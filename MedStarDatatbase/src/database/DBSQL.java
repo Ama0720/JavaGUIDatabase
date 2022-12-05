fd575b4f4af1e29b235ee13eaacf77d22f8c5df8
bwhitt7 <brit12201@gmail.com>
2022-12-05 17:19:10 -0500

    updates

--- MedStarDatatbase/src/database/DBSQL.java
+++ MedStarDatatbase/src/database/DBSQL.java
@@ -50,6 +50,31 @@ public class DBSQL {
         return rs;
     }//Main
 
+    public void insert(String query){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+        }catch (ClassNotFoundException e){
+            System.out.println(e);
+        }
+    
+        final String ID = "bwhitt7";
+        final String PW = "COSC*4a53h";
+        final String SERVER = "jdbc:mysql://triton.towson.edu:3360/?serverTimezone=EST#/bwhitt7db";
+        try {
+            Connection con = DriverManager.getConnection(SERVER, ID, PW);
+            Statement stmt = con.createStatement();
+            stmt.execute("use bwhitt7db");
+            //rs = stmt.executeQuery(query);
+            PreparedStatement insert;
+
+            //querys = "UPDATE shongdb.Branch SET city = 'Baltimore' WHERE branchNo = 'B002';";
+            insert = con.prepareStatement(query);
+            insert.executeUpdate();
+        }catch (SQLException e){
+            System.err.println(e);
+        }
+    }
 
     public DefaultTableModel makeTable(ResultSet rs){
         DefaultTableModel m = new DefaultTableModel();
@@ -75,6 +100,43 @@ public class DBSQL {
         
         return m;
 
+    }
+    
+    public DefaultTableModel makeDataTable(ResultSet rs){
+        DefaultTableModel m = new DefaultTableModel();
+
+        try {
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int rows = rsmd.getColumnCount();
+            m.addColumn("Data");
+            m.addColumn("Value");
+
+            while(rs.next()){
+                for (int i = 0; i < rows; i++){
+                    String[] row = new String[2];
+                    row[0] = rsmd.getColumnLabel(i+1);
+                    row[1] = rs.getString(i+1);
+                    m.addRow(row);
+                }
+            }
+        }catch (SQLException e){
+            System.err.println(e);
+        }
+        
+        return m;
+    }
+    
+    public String[] getList(ResultSet rs){
+        ArrayList<String> list = new ArrayList<>();
+        list.add(null);
+        try {
+            while(rs.next()){
+                list.add(rs.getString(1));
+            }
+        }catch (SQLException e){
+            System.err.println(e);
+        }
+        return list.toArray(new String[list.size()]);
     }
 
 }
